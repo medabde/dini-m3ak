a912ec72b3805e8941ec4477bc27f4294c4a12e8
DESKTOP-FA8S9OS\Admin <wafaeissa17@gmail.com>
2021-04-08 10:28:57 +0200

    profil

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import {NavbarComponent} from './navbar/navbar.component';
 import {HeaderComponent} from './header/header.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {ProfilComponent} from './profil/profil.component';
+
 const routes: Routes = [
 { path: 'login', component: LoginComponent },
 { path: 'register', component: RegisterComponent },
 { path: 'header', component: HeaderComponent },
+{ path: 'Profil', component: ProfilComponent },
 ];
 
 @NgModule({
--- client/src/app/profil/profil.component.ts
+++ client/src/app/profil/profil.component.ts
@@ -1,13 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.css']
+  styleUrls: ['./profil.component.scss']
 })
 export class ProfilComponent implements OnInit {
+  public userInfo = [];
 
-  constructor() { }
+  /**
+   * Nome
+   */
+  public name = '';
+
+  /**
+   * RG
+   */
+  public geral_register = '';
+
+  /**
+   * Endereço
+   */
+  public address = '';
+
+  /**
+   * Complemento
+   */
+  public complement = '';
+
+  /**
+   * CEP
+   */
+  public ZIP = '';
+
+  /**
+   * Bairro
+   */
+  public neighborhood = '';
+
+  /**
+   * Cidade
+   */
+  public city = '';
+
+  /**
+   * UF
+   */
+  public federal_unit = '';
+
+  /**
+   * Telefone
+   */
+  public phone = '';
+
+  /**
+   * Celular
+   */
+  public celphone = '';
+
+  /**
+   * Settings Constructor
+   *
+   * @param { UsersService } users - Users HTTP Service
+   * @param { Router } router - Angular Router
+   */
+  constructor(
+    private router: Router,
+  ) { }
+
+
+ 
 
   ngOnInit(): void {
   }
