dc228b70c8480abb9acb9d38b1b123d3a8f50f1b
abirmach <abirmach.99@gmail.com>
2021-04-10 22:13:49 +0200

    Login

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,41 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  validatingForm:any;
-  faeye=faEye;
- /**
-   * Username Field
-   */
-  public username = '';
-
-  /**
-   * Password Field
-   */
-  public password = '';
-
-  /**
-   * Visibility Boolean
-   */
-  public isVisible = false;
-
-  /**
-   * message Error
-   */
-  public messageError: string ='';
-  public emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
 
   constructor() { }
 
   ngOnInit(): void {
-    this.validatingForm = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email])
-    });
   }
-  }
+
+}
