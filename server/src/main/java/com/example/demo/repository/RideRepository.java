c0d85fc6d78299f25550cced341275bd36a9abce
medabde <medabde9@gmail.com>
2021-04-11 12:16:34 +0000

    controllers logic changes

--- server/src/main/java/com/example/demo/repository/RideRepository.java
+++ server/src/main/java/com/example/demo/repository/RideRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Ride;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +9,10 @@ import java.util.List;
 
 public interface RideRepository extends JpaRepository<Ride, Long> {
     @Query("SELECT r FROM Ride r WHERE r.isEnabled = true")
-    List<Ride> findEnabledRies();
+    List<Ride> findEnabledRides();
+
+    @Query("SELECT r FROM Ride r WHERE r.user = user AND r.isEnabled = true")
+    List<Ride> findEnabledRidesByUser(User user);
+
+
 }
