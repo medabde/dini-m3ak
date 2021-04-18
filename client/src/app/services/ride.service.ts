121e4ffc677ad662fd39f9e4bf4f4272dc3ac982
DESKTOP-FA8S9OS\Admin <wafaeissa17@gmail.com>
2021-04-18 23:13:27 +0200

    ride-component-back

--- client/src/app/services/ride.service.ts
+++ client/src/app/services/ride.service.ts
@@ -16,7 +16,14 @@ export class RideService {
 
     return this.http.get<Ride[]>("/app/api/rides/enabled",{headers:this.getHeader()});
   }
+  getRidesbyid(id_ride:number):Observable<Ride[]>{
+    var rides:Ride[] = [];
 
+    return this.http.get<Ride[]>("/app/api/rides/"+id_ride,{headers:this.getHeader()});
+  }
+  deleteList(_id:any):Observable<Ride>{
+    return this.http.delete<Ride>(`/app/api/rides/${_id}`,{headers:this.getHeader()});
+  }
   joinRide(id_ride:number):Observable<Ride>{
     
     return this.http.put<Ride>("/app/api/rides/joinride/"+id_ride,{},{headers:this.getHeader()});
