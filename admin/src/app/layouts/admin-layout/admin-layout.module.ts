f1c2d8adf4a9807cfeaa287f624da02bc8aa292d
Bahae Eddine Bouzeggaoui <bouzeggaouibahaeeddine@gmail.com>
2021-05-23 18:55:14 +0100

    dashbord

--- admin/src/app/layouts/admin-layout/admin-layout.module.ts
+++ admin/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -8,10 +8,6 @@ import { AdminLayoutRoutes } from "./admin-layout.routing";
 import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
 import { UserComponent } from "../../pages/user/user.component";
 import { TableComponent } from "../../pages/table/table.component";
-import { TypographyComponent } from "../../pages/typography/typography.component";
-import { IconsComponent } from "../../pages/icons/icons.component";
-import { MapsComponent } from "../../pages/maps/maps.component";
-import { NotificationsComponent } from "../../pages/notifications/notifications.component";
 import { UpgradeComponent } from "../../pages/upgrade/upgrade.component";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
@@ -29,11 +25,7 @@ import { RidesComponent } from "app/pages/rides/rides.component";
     UserComponent,
     TableComponent,
     UpgradeComponent,
-    TypographyComponent,
     RidesComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
   ],
 })
 export class AdminLayoutModule {}
--- admin/src/app/sidebar/sidebar.component.ts
+++ admin/src/app/sidebar/sidebar.component.ts
@@ -9,22 +9,11 @@ export interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
   { path: "/dashboard", title: "Dashboard", icon: "nc-bank", class: "" },
-  { path: "/icons", title: "Icons", icon: "nc-diamond", class: "" },
-  { path: "/maps", title: "Maps", icon: "nc-pin-3", class: "" },
-  {
-    path: "/notifications",
-    title: "Notifications",
-    icon: "nc-bell-55",
-    class: "",
-  },
+
+
   { path: "/user", title: "User Profile", icon: "nc-single-02", class: "" },
   { path: "/table", title: "Table List", icon: "nc-tile-56", class: "" },
-  {
-    path: "/typography",
-    title: "Typography",
-    icon: "nc-caps-small",
-    class: "",
-  },
+
   { path: "/rides", title: "Rides", icon: "nc-tile-56", class: "" },
   // { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
 ];
