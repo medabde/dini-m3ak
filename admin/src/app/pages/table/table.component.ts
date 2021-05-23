3b0b8725fedfea11bc5385614b9987a9e54bae82
Bahae Eddine Bouzeggaoui <bouzeggaouibahaeeddine@gmail.com>
2021-05-23 10:59:21 +0100

    table.ts

---
+++ admin/src/app/pages/table/table.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/User';
+
+declare interface TableData {
+    headerRow: string[];
+    dataRows: any;
+}
+
+@Component({
+    selector: 'table-cmp',
+    moduleId: module.id,
+    templateUrl: 'table.component.html'
+})
+
+export class TableComponent implements OnInit{
+    public tableData1: TableData;
+    users : User[];
+  constructor(private userService: UserService, private router: Router ) { }
+    ngOnInit(){
+        this.getUsers();
+
+  }
+  private getUsers(){
+    this.userService.getAllUsers().subscribe(data => { this.users = data;
+      console.log(this.users)
+    })
+  }
+
+  modifier(id){
+    console.log(id);
+    this.router.navigate(['update-user', id]);
+  }
+  /*
+  updateUser(id: number){
+    this.router.navigate(['update-user', id]);
+  }
+    */
+  deleteRide(id: number) {
+    this.userService.deleteUser(id).subscribe((data) => {
+      this.getUsers();
+    });
+  }
+  }
